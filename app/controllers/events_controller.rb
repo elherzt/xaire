ef0cf03c25d085a2cabe9287292af0f273c9c65d
Jesus Romero <elherzt@gmail.com>
2013-11-05 13:01:12 -0600

    add created method with decent exposure

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,38 @@
 class EventsController < ApplicationController
+  respond_to :html
+  expose(:user) { User.find(params[:user_id]) }
+  expose(:events) do 
+    user.events.where(params.except(:action, :controller, :format, :utf8, :commit, :authenticity_token, :user_id))
+  end
   expose(:event)
+
+
+  def new
+    #@user = User.find(params[:user_id])
+    #@event = @user.events.new
+  end
+
+  def create
+
+    #@user = User.find(params[:user_id])
+    #@event = @user.events.build(event_params)
+    event.save
+    respond_with(event)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :name,
+      :description,
+      :date,
+      :place,
+      :cost,
+      :hour,
+      :requirements,
+      :notes,
+      :user_id
+    )
+  end
 end
