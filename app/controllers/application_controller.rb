ce2d25b6b11d1f9828140ffe221e9a5c712993dd
Jesus Romero <elherzt@gmail.com>
2013-11-05 13:00:40 -0600

    added configuration to strong params in decent exposure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
  before_filter :configure_permitted_parameters, if: :devise_controller?
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+ protect_from_forgery with: :exception
 
+decent_configuration do
+  strategy DecentExposure::StrongParametersStrategy
+end
 
-
-  protected
+protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :email
     devise_parameter_sanitizer.for(:sign_up) << :username 
     devise_parameter_sanitizer.for(:sign_in) << :username 
   end
