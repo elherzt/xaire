5d7eda8249ae83e9598760fee25d41e850850181
Jesus Romero <elherzt@gmail.com>
2013-11-26 13:19:04 -0600

    created table events

--- db/migrate/20131104232405_create_table_events.rb
+++ db/migrate/20131104232405_create_table_events.rb
@@ -1,16 +1,16 @@
 class CreateTableEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.integer :user_id
-      t.string  :name
-      t.string  :description
-      t.string  :start_date, 
-      t.string  :end_date,   
-      t.string  :place
-      t.string  :cost
-      t.string  :hour
-      t.string  :requirements
-      t.string  :notes
+      t.integer   :user_id
+      t.string    :name
+      t.string    :description
+      t.datetime  :start_date 
+      t.datetime  :end_date  
+      t.string    :place
+      t.string    :cost
+      t.string    :hour
+      t.string    :requirements
+      t.string    :notes
       t.timestamps
     end
   end
